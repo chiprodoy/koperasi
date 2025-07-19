64744260c6226c3f60f499e868803e4d668c0234
chiprodoy <chiprodoy@gmail.com>
2025-07-20 05:44:22 +0700

    pembuatan model jenis simpanan

---
+++ database/migrations/2025_07_19_223841_create_jenis_simpanans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jenis_simpanans', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('nama_jenis_simpanan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jenis_simpanans');
+    }
+};
