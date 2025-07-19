b386e1a3a2e7c76346b7275ecf162d648b08eac0
chiprodoy <chiprodoy@gmail.com>
2025-07-20 04:35:01 +0700

    pembuatan migrasi anggota

---
+++ database/migrations/2025_07_19_212808_create_anggotas_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anggotas', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('nomor_anggota');
+            $table->string('nik');
+            $table->string('nama');
+            $table->integer('jenis_kelamin');
+            $table->string('alamat');
+            $table->string('kelurahan');
+            $table->string('kecamatan');
+            $table->string('kota');
+            $table->string('provinsi');
+            $table->char('telepon', 12);
+            $table->date('tgl_mulai_anggota');
+            $table->string('nama_ibu');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anggotas');
+    }
+};
