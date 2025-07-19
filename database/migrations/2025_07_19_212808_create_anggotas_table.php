70a1b7dd7ae50eed8c48de6facd1a5ac0958d12e
chiprodoy <chiprodoy@gmail.com>
2025-07-20 05:34:33 +0700

    pembuatan migrasi anggota

--- database/migrations/2025_07_19_212808_create_anggotas_table.php
+++ database/migrations/2025_07_19_212808_create_anggotas_table.php
@@ -16,18 +16,22 @@ return new class extends Migration
         Schema::create('anggotas', function (Blueprint $table) {
             $table->id();
             $table->uuid();
-            $table->string('nomor_anggota');
-            $table->string('nik');
+            $table->string('nomor_anggota')->unique();
+            $table->string('nik')->unique();
             $table->string('nama');
+            $table->string('tgl_lahir');
             $table->integer('jenis_kelamin');
+            $table->string('pekerjaan');
             $table->string('alamat');
             $table->string('kelurahan');
             $table->string('kecamatan');
             $table->string('kota');
             $table->string('provinsi');
             $table->char('telepon', 12);
-            $table->date('tgl_mulai_anggota');
             $table->string('nama_ibu');
+            $table->date('tgl_mulai_anggota');
+            $table->integer('status_keanggotaan'); // 1 = aktif
+            $table->integer('jenis_keanggotaan'); // 1 = pengurus 2=anggota
             $table->timestamps();
             $table->softDeletes();
         });
