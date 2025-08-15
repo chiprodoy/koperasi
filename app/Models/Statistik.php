31a0af5bd5a846e426a33406906e53a097b887c7
chiprodoy <chiprodoy@gmail.com>
2025-08-15 07:55:54 +0700

    add crud statistik

--- app/Models/Statistik.php
+++ app/Models/Statistik.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Statistik extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+     protected $fillable = [
+        'uuid',
+        'slug',
+        'icon',
+        'label',
+        'jumlah'
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->uuid)) {
+                $model->uuid = Str::uuid();
+            }
+        });
+    }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StatistikController;
 use App\Http\Controllers\TestimoniController;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
@@ -116,4 +117,5 @@ Route::prefix('/admin')->middleware(['auth'])->group(function () {
         Route::delete('/delete/{uuid}', [App\Http\Controllers\HargaSawitController::class,'destroy'])->name('admin-hasil-panen.destroy');
     });
     Route::resource('testimoni', TestimoniController::class, ['names' => 'admin.testimoni']);
+    Route::resource('statistik', StatistikController::class, ['names' => 'admin.statistik']);
 });
